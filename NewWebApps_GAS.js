535bb41147048b7e71f3d7752fd0f5ad86ad7d6c
talaw-K <44435865+talaw-K@users.noreply.github.com>
2019-01-13 17:34:05 +0900

    CSS分離

--- NewWebApps_GAS.js
+++ NewWebApps_GAS.js
@@ -21,3 +21,34 @@ HtmlOutputオブジェクト
 
 
 */
+
+/*
+<!-- スクリプトタグは最後の方に -->
+<!-- JS　記載場所 -->
+<!-- ここから -->
+<script>
+function changeMessage() {
+document.getElementById('hello').textContent = 'Hello JavaScript!';
+}
+</script>
+<!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+
+<!-- ここまで -->
+
+<!-- 外部ファイル -->
+<!-- 
+<link rel="styleshee" href="CSSファイルのURL">
+<script src="JavaScriptファイルのURL"></script>
+ -->
+ 
+ <!--
+ Content Delivery Network=ネット上で公開されている
+ コンテンツやライブラリを効率よく配布するための仕組み
+ HTML内にリンク情報などをコピペするだけで
+ 配布物を使用できる
+-->
+*/
