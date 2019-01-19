39abf523cabb9b8f14e547597ae644243cb21fd4
talaw-K <44435865+talaw-K@users.noreply.github.com>
2019-01-19 20:58:50 +0900

    test

--- NewWebApps/info.php
+++ NewWebApps/info.php
@@ -14,20 +14,35 @@ echo '私は'.$name.'です'.'<br>';
 $blog = mktime(15,30,0,2,10,2014);
 
 if($blog>=time()-86400){
-    echo 'New!';
+echo 'New!';
 }else if($blog==time()-86400){
-    echo'soon';
+echo'soon';
 }else{
-    echo'yet';
+echo'yet';
 }
 
 for ($i = 1;$i<=10;$i++){
-    echo $i."<br>";
+echo $i."<br>";
 }
 
 $i = 1;
 while ($i <= 10){
-    echo $i.'<br>';
-    $i++;
+echo $i.'<br>';
+$i++;
 }
+
+$i=0
+switch($1){
+case 0:
+echo 'A';
+break;
+case 1:
+echo 'B';
+break;
+case 2:
+echo 'C'
+break;
+}
+
+
 ?>
